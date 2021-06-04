a969dabe483feeab1caffa1bfdea9fcb4357c085
Kenny Lam <kinwai.lam730@gmail.com>
2021-06-04 00:17:59 -0400

    Added markdown folder and markdown file and added path in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/markdown`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
---
+++ src/markdown/firstpost.md
@@ -0,0 +1,11 @@
+---
+title: 'My first Gatsby page'
+date: 'June 4th, 2021'
+---
+
+###Learning Gatsby
+
+Welcome to my first Gatsby page!
+I am experimenting with Gatsby on this project and trying different things.
+I am also learning some GraphQL as well.
+Thanks for checking out my first post!
