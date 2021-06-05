e62ed161aae3cb56cb4f223968ee63787623de5e
Kenny Lam <kinwai.lam730@gmail.com>
2021-06-04 22:14:01 -0400

    Wrote out gatsby-node file, able to generate slugs for pages when we start the app

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,23 @@
  */
 
 // You can delete this file if you're not using it
+
+//we are creating a slug. A slug is essentially just a navigation path inside the app that allows us to route to it 
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+//the arguments are the actual nodes, such as images, markdowns like this app. getNode is the method to grab the nodes. Actions are a list of built in action in Gatsby
+exports.onCreateNode = ({node, getNode, actions}) => {
+    //this is to build a path dynamically. 
+    const { createNodeField } = actions
+    //on start up on app we can see the types of files that are being built up
+    console.log(node.internal.type)
+    if(node.internal.type === `MarkdownRemark`){
+        //creating the route here
+        const slug = createFilePath({ node, getNode })
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug
+        })
+    }
+}
